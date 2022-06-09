2dd584a59184cb51eed465746ae1e826c1dbe669
ManuelCebreiro <cebreirom@gmail.com>
2022-06-09 23:40:19 +0000

    Aqui esta mi ejercicio de generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#excuses").innerHTML = Escusas();
+
+  function Escusas() {
+    let who = ["Un vagabundo", "Mi tio", "Mi perro", "Mi suegra"];
+    let what = ["se comió", "rompió", "ensució", "robó"];
+    let when = [
+      "mientras yo dormía",
+      "mientras yo no miraba",
+      "cuando estaba en la ducha"
+    ];
+
+    let whoramdon = Math.floor(Math.random() * who.length);
+    let whatramdon = Math.floor(Math.random() * what.length);
+    let whenramdon = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoramdon] +
+      " " +
+      what[whatramdon] +
+      " mi trabajo " +
+      " " +
+      when[whenramdon]
+    );
+  }
 };
