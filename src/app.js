5f31f1b52091484b5df105b9b02c18b3c043ccff
ManuelCebreiro <cebreirom@gmail.com>
2022-06-13 14:39:23 +0000

    botones añadidos

--- src/app.js
+++ src/app.js
@@ -5,30 +5,54 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const who = ["Un vagabundo", "Mi tio", "Mi perro", "Mi suegra"];
+const theacction = ["se comió", "rompió", "ensució", "robó"];
+const what = ["mi trabajo", "mi móvil", "mi consola", "mis gafas"];
+const when = [
+  "mientras yo dormía",
+  "mientras yo no miraba",
+  "cuando estaba en la ducha",
+  "cuando yo no estaba en casa"
+];
+
 window.onload = () => {
+  const whofrase = Escusas(who);
+  const theacctionfrase = Escusas(theacction);
+  const whatfrase = Escusas(what);
+  const whenfrase = Escusas(when);
+
   document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#excuses").innerHTML = Escusas();
+    document.querySelector("#excuses").innerHTML = `${Escusas(who)} ${Escusas(
+      theacction
+    )} ${Escusas(what)} ${Escusas(when)}`;
   });
-  function Escusas() {
-    let who = ["Un vagabundo", "Mi tio", "Mi perro", "Mi suegra"];
-    let what = ["se comió", "rompió", "ensució", "robó"];
-    let when = [
-      "mientras yo dormía",
-      "mientras yo no miraba",
-      "cuando estaba en la ducha"
-    ];
 
-    let whoramdon = Math.floor(Math.random() * who.length);
-    let whatramdon = Math.floor(Math.random() * what.length);
-    let whenramdon = Math.floor(Math.random() * when.length);
+  document.querySelector("#btnwho").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = `${whofrase} ${Escusas(
+      theacction
+    )} ${Escusas(what)} ${Escusas(when)}`;
+  });
 
-    return (
-      who[whoramdon] +
-      " " +
-      what[whatramdon] +
-      " mi trabajo " +
-      " " +
-      when[whenramdon]
-    );
+  document.querySelector("#btntheacction").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = `${Escusas(
+      who
+    )} ${theacctionfrase} ${Escusas(what)} ${Escusas(when)}`;
+  });
+
+  document.querySelector("#btnwhat").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = `${Escusas(who)} ${Escusas(
+      theacction
+    )} ${whatfrase} ${Escusas(when)}`;
+  });
+
+  document.querySelector("#btnwhen").addEventListener("click", () => {
+    document.querySelector("#excuses").innerHTML = `${Escusas(who)} ${Escusas(
+      theacction
+    )} ${Escusas(what)} ${whenfrase}`;
+  });
+
+  function Escusas(str) {
+    const aleatorio = Math.floor(Math.random() * str.length);
+    return str[aleatorio];
   }
 };
